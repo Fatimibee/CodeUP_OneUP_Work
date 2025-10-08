9e2a1f503343131bf2437697de5a4a37afdca143
Fatimibee <FATIMIBEE312@gmail.com>
2025-10-08 22:30:24 +0530

    All Changes done

--- Main.java
+++ Main.java
@@ -10,24 +10,24 @@ import java.util.*;
 public class Main {
 
       public static void main(String[] args) {
-        Scanner scannerObj=new Scanner(System.in);
+        Scanner scannerObj=new Scanner(System.in) ;
 
         // String input
-        System.out.println("Enter a String:");
-        String str=scannerObj.nextLine();
+        System.out.println("Enter a String:") ;
+        String str=scannerObj.nextLine() ;
         
-        MyString objMyString=new MyString(str);
+        MyString objMyString=new MyString(str) ;
         
 
         // menu
-        System.out.println("Choices:");
-        System.out.println(" 1. Append a string \n 2. Count words \n 3. Replace \n 4. Is palindrome \n 5. Splice \n 6. Split \n 7. Max Reapeting Character \n 8. Sort \n 9. Shift \n 10. Reverse \n 11. Exit");
+        System.out.println("Choices:") ;
+        System.out.println(" 1. Append a string \n 2. Count words \n 3. Replace \n 4. Is palindrome \n 5. Splice \n 6. Split \n 7. Max Reapeting Character \n 8. Sort \n 9. Shift \n 10. Reverse \n 11. Exit") ;
         
         // choice enter
-        System.out.println("Enter your choice:");
-        int choice=scannerObj.nextInt();
+        System.out.println("Enter your choice:") ;
+        int choice=scannerObj.nextInt() ;
 
-        scannerObj.nextLine();
+        scannerObj.nextLine() ;
 
 
         // calling function based on user choice
@@ -35,107 +35,107 @@ public class Main {
             
             // append choice
             case 1:
-                System.out.println("Enter a string to append:");
-                String append_str = scannerObj.nextLine();
-                String ans = objMyString.append_Func(append_str);
-                System.out.println("Result after appending : " + ans);
-                break;
+                System.out.println("Enter a string to append:") ;
+                String append_str = scannerObj.nextLine() ;
+                String ans = objMyString.append_Func(append_str) ;
+                System.out.println("Result after appending : " + ans) ;
+                break ;
 
 
             // count word choice
             case 2:
-                int words = objMyString.count();
-                System.out.println("Number of words in String : " + words);
-                break;
+                int words = objMyString.count() ;
+                System.out.println("Number of words in String : " + words) ;
+                break ;
 
 
             // replace choice
             case 3:
-               System.out.println("Enter old character: ");
-               char oldChar = scannerObj.next().charAt(0);
+               System.out.println("Enter old character: ") ;
+               char oldChar = scannerObj.next().charAt(0) ;
 
-               System.out.println("Enter new character: ");
-               char newChar = scannerObj.next().charAt(0);
+               System.out.println("Enter new character: ") ;
+               char newChar = scannerObj.next().charAt(0) ;
 
-                objMyString.replace_Func(oldChar, newChar);
-                System.out.println("Result after replacing : " + objMyString.value);
-                break;
+                objMyString.replace_Func(oldChar, newChar) ;
+                System.out.println("Result after replacing : " + objMyString.value) ;
+                break ;
 
 
             // is palindrom choice
             case 4:
-            boolean isPal = objMyString.is_Palindrom();
+            boolean isPal = objMyString.is_Palindrom() ;
                 if(isPal){
-                    System.out.println("String is palindrom");
+                    System.out.println("String is palindrom") ;
                 }
                 else{
-                    System.out.println("String is not palindrom");
+                    System.out.println("String is not palindrom") ;
                 }
-               break;
+               break ;
 
 
             // spice choice
             case 5:
-                System.out.println("Enter start index: ");
-                int start = scannerObj.nextInt();
+                System.out.println("Enter start index: ") ;
+                int start = scannerObj.nextInt() ;
 
-                System.out.println("Enter length of splice : ");
-                int length = scannerObj.nextInt();
+                System.out.println("Enter length of splice : ") ;
+                int length = scannerObj.nextInt() ;
 
-                String spliced = objMyString.splice_Func(start,length);
-                System.out.println("Result after splicing : " + spliced);
-                break;
+                String spliced = objMyString.splice_Func(start,length) ;
+                System.out.println("Result after splicing : " + spliced) ;
+                break ;
 
 
             // split choice
             case 6:
-            System.out.println("Enter pattern to split:");
-            String Pattern_str = scannerObj.nextLine();
-            objMyString.split_Func(Pattern_str);
-            break;
+            System.out.println("Enter pattern to split:") ;
+            String Pattern_Str = scannerObj.nextLine() ;
+            objMyString.split_Func(Pattern_Str) ;
+            break ;
 
 
             // max repeating char choice
             case 7:
-                objMyString.get_Max_Repeating_Char();
-                break;
+                objMyString.get_Max_Repeating_Char() ;
+                break ;
 
 
             // sort choice
             case 8:
-                String sorted = objMyString.sort_Func();
-                System.out.println("Result after sorting : " + sorted);
-                break;
+                String sorted = objMyString.sort_Func() ;
+                System.out.println("Result after sorting : " + sorted) ;
+                break ;
 
 
             // shift choice
             case 9:
-               System.out.println("Enter number of elements to shift at end: ");
-               int n = scannerObj.nextInt();
-               String shifted = objMyString.shift_Func(n);
-               System.out.println("Result after shifting : " + shifted);
-                break;
+               System.out.println("Enter number of elements to shift at end: ") ;
+               int n = scannerObj.nextInt() ;
+               String shifted = objMyString.shift_Func(n) ;
+               System.out.println("Result after shifting : " + shifted) ;
+                break ;
 
 
             // reverse choice
             case 10:
-                String reversed = objMyString.reverse_Func();
-                System.out.println("Result after reversing : " + reversed);
-                break;
+                String reversed = objMyString.reverse_Func() ;
+                System.out.println("Result after reversing : " + reversed) ;
+                break ;
 
 
             // exit choice
             case 11:
-                System.exit(0);
-                break;
+                System.exit(0) ;
+                break ;
 
                 
             // default case
             default:
-                 System.out.println("You entered invalid choice");
-                 break;
+                 System.out.println("You entered invalid choice") ;
+                 break ;
         }
-        scannerObj.close();
+        scannerObj.close() ;
 
     }
 };
--- MyString.java
+++ MyString.java
@@ -7,28 +7,28 @@
 import java.util.*;
 // MyString class
 class MyString {
-    String value = "Hello";
+    String value = "Hello" ;
     MyString(String value) {
-       this.value=value;
+       this.value = value ;
     }
 
 
     // 1. Append function
     public String append_Func(String append_str){
-        String result = this.value +" "+ append_str;
-        return result;
+        String result = this.value +" "+ append_str ;
+        return result ;
     }
 
 
     // 2. count words function
     public int count(){
        int result = 1;
-       for(int i = 0 ;i < this.value.length() ; i++){
+       for(int i = 0 ; i < this.value.length() ; i++){
         if (this.value.charAt(i) == ' ') {
-                result++;
+                result++ ;
            }
        }
-      return result;
+      return result ;
     }
 
 
@@ -36,7 +36,7 @@ class MyString {
     public void replace_Func( char oldChar, char newChar){
          for(int i = 0 ; i < this.value.length() ; i++){
             if(this.value.charAt(i) == oldChar){
-                this.value = this.value.substring(0,i)+newChar+this.value.substring(i+1,this.value.length());
+                this.value = this.value.substring(0,i)+newChar+this.value.substring(i+1,this.value.length()) ;
             }
          }
          
@@ -45,59 +45,59 @@ class MyString {
 
     // 4. chk is string palindrom or not
     public boolean is_Palindrom(){
-        String str=this.value;
-        int i = 0;
-        int j = str.length() - 1;
+        String str=this.value ;
+        int i = 0 ;
+        int j = str.length() - 1 ;
 
         while (i < j) {
             if (str.charAt(i) != str.charAt(j)) {
-                return false;
+                return false ;
             }
-            i++;
-            j--;
+            i++ ;
+            j-- ;
         }
 
-        return true;
+        return true ;
     }
 
 
     // 5. splice function
     public String splice_Func(int start, int length){
        if(start < 0 || start >= this.value.length()  || length < 0 || start + length > this.value.length()){
-        return this.value;
+        return this.value ;
        }
-       String result = this.value.substring(0 , start) + this.value.substring(start + length , this.value.length());
-       return result;
+       String result = this.value.substring(0 , start) + this.value.substring(start + length , this.value.length()) ;
+       return result ;
     }
 
 
     // 6. split function
     public void split_Func(String pattern){
-        String str=this.value;
+        String str=this.value ;
         ArrayList<String> words = new ArrayList<>();
-        String answer = "";
-        for(int i=0;i<str.length(); ){
+        String answer = "" ;
+        for(int i = 0 ; i < str.length() ; i++){
             // check if the pattern matches at the current position (supports multi-char pattern)
             if(pattern != null && pattern.length() > 0 && i + pattern.length() <= str.length()
                && str.substring(i, i + pattern.length()).equals(pattern)){
                 if(!answer.isEmpty()){
-                    words.add(answer);
-                    answer = "";
+                    words.add(answer) ;
+                    answer = "" ;
                 }
-                i += pattern.length();
+                i += pattern.length() ;
             } else {
-                answer += str.charAt(i);
-                i++;
+                answer += str.charAt(i) ;
+                i++ ;
             }
         }
 
         if(!answer.isEmpty()){
-            words.add(answer);
+            words.add(answer) ;
         }
 
-        System.out.println("Result after splitting :");
+        System.out.println("Result after splitting : ") ;
         for(String word: words){
-            System.out.print(word + "\t");
+            System.out.print(word + "\t") ;
         }
 
     }
@@ -105,66 +105,66 @@ class MyString {
 
     // 7. max repeating char function
     public void  get_Max_Repeating_Char(){
-        int[] count = new int[256];
-        String str = this.value;
+        int[] count = new int[256] ;
+        String str = this.value ;
         for(int i = 0 ; i < str.length() ; i++){
-            count[str.charAt(i)]++;
+            count[str.charAt(i)]++ ;
         }
-        int max = 0;
-        char result = ' ';
-        for (int i = 0; i < 256; i++) {
+        int max = 0 ;
+        char result = ' ' ;
+        for (int i = 0 ; i < 256 ; i++) {
             if (count[i] > max) {
-            max = count[i];
-            result = (char) i;
+            max = count[i] ;
+            result = (char) i ;
             }
         }
-        System.out.println("Max Repeating character in String is : "+result+" with frequency :"+max);
+        System.out.println("Max Repeating character in String is : "+result+" with frequency :"+max) ;
 
     }
 
 
     // 8. sort function
     public String sort_Func() {
-        String str=this.value;
+        String str=this.value ;
         
-    char[] arr = str.toCharArray(); 
+    char[] arr = str.toCharArray() ; 
     
-    for (int i = 0; i < arr.length - 1; i++) {
-        for (int j = i + 1; j < arr.length; j++) {
+    for (int i = 0 ; i < arr.length - 1 ; i++) {
+        for (int j = i + 1 ; j < arr.length ; j++) {
             if (arr[i] > arr[j]) {
-                char temp = arr[i];
-                arr[i] = arr[j];
-                arr[j] = temp;
+                char temp = arr[i] ;
+                arr[i] = arr[j] ;
+                arr[j] = temp ;
             }
         }
     }
-    String sortedStr = new String(arr);
-    return sortedStr;
+    String sortedStr = new String(arr) ;
+    return sortedStr ;
     }
 
 
     // 9. shift function
     public String shift_Func(int n){
-    String str=this.value;
-    n = n % str.length();
+    String str=this.value ;
+    n = n % str.length() ;
     if(n == 0) {
        
         return str ;
     }
-    String result = str.substring(str.length() - n , str.length()) + str.substring(0 , str.length() - n);
-    return result;
+    String result = str.substring(str.length() - n , str.length()) + str.substring(0 , str.length() - n) ;
+    return result ;
     }
 
 
     // 10. reverse function
     public String reverse_Func(){
-        String str = this.value;
-        String result = "";
+        String str = this.value ;
+        String result = "" ;
         for(int i = str.length()-1 ; i >= 0 ; i--){
-            result += str.charAt(i);
+            result += str.charAt(i) ;
         }
 
-        return result;
+        return result ;
     }
 
 };
