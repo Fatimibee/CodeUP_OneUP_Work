0858d9e96bfbaff2364d6983db76f06b16ae614a
Fatimibee <FATIMIBEE312@gmail.com>
2025-10-10 14:07:34 +0530

    Try block added

--- Main.java
+++ Main.java
@@ -1,141 +1,128 @@
- /**
-  *@filename- Main.java
-  *@description -This is main file which contain main function and menu to call different functions of MyString class.
-  *@author - Fatimi Bee
-  */
-
+/**
+ * @filename - Main.java
+ * @description - This is the main file which contains the main function 
+ *                and menu to call different functions of MyString class.
+ * @author - Fatimi Bee
+ */
 
 import java.util.*;
 
 public class Main {
 
-      public static void main(String[] args) {
-        Scanner scannerObj=new Scanner(System.in) ;
+    public static void main(String[] args) {
+        Scanner scannerObj = new Scanner(System.in);
 
-        // String input
-        System.out.println("Enter a String:") ;
-        String str=scannerObj.nextLine() ;
-        
-        MyString objMyString=new MyString(str) ;
-        
+        try {
+            // String input
+            System.out.println("Enter a String:");
+            String str = scannerObj.nextLine();
 
-        // menu
-        System.out.println("Choices:") ;
-        System.out.println(" 1. Append a string \n 2. Count words \n 3. Replace \n 4. Is palindrome \n 5. Splice \n 6. Split \n 7. Max Reapeting Character \n 8. Sort \n 9. Shift \n 10. Reverse \n 11. Exit") ;
-        
-        // choice enter
-        System.out.println("Enter your choice:") ;
-        int choice=scannerObj.nextInt() ;
+            MyString objMyString = new MyString(str);
 
-        scannerObj.nextLine() ;
+            // menu
+            System.out.println("Choices:");
+            System.out.println(" 1. Append a string"
+                    + "\n 2. Count words"
+                    + "\n 3. Replace"
+                    + "\n 4. Is palindrome"
+                    + "\n 5. Splice"
+                    + "\n 6. Split"
+                    + "\n 7. Max Repeating Character"
+                    + "\n 8. Sort"
+                    + "\n 9. Shift"
+                    + "\n 10. Reverse"
+                    + "\n 11. Exit");
 
+            // choice enter
+            System.out.println("Enter your choice:");
+            int choice = scannerObj.nextInt();
+            scannerObj.nextLine(); // consume newline
 
-        // calling function based on user choice
-        switch(choice){
-            
-            // append choice
-            case 1:
-                System.out.println("Enter a string to append:") ;
-                String append_str = scannerObj.nextLine() ;
-                String ans = objMyString.appendFunc(append_str) ;
-                System.out.println("Result after appending : " + ans) ;
-                break ;
+            // calling function based on user choice
+            switch (choice) {
 
+                case 1:
+                    System.out.println("Enter a string to append:");
+                    String append_str = scannerObj.nextLine();
+                    String ans = objMyString.appendFunc(append_str);
+                    System.out.println("Result after appending: " + ans);
+                    break;
 
-            // count word choice
-            case 2:
-                int words = objMyString.count() ;
-                System.out.println("Number of words in String : " + words) ;
-                break ;
+                case 2:
+                    int words = objMyString.count();
+                    System.out.println("Number of words in String: " + words);
+                    break;
 
+                case 3:
+                    System.out.println("Enter old character: ");
+                    char oldChar = scannerObj.next().charAt(0);
+                    System.out.println("Enter new character: ");
+                    char newChar = scannerObj.next().charAt(0);
+                    objMyString.replaceFunc(oldChar, newChar);
+                    System.out.println("Result after replacing: " + objMyString.value);
+                    break;
 
-            // replace choice
-            case 3:
-               System.out.println("Enter old character: ") ;
-               char oldChar = scannerObj.next().charAt(0) ;
+                case 4:
+                    boolean isPal = objMyString.isPalindrom();
+                    if (isPal) {
+                        System.out.println("String is palindrome");
+                    } else {
+                        System.out.println("String is not palindrome");
+                    }
+                    break;
 
-               System.out.println("Enter new character: ") ;
-               char newChar = scannerObj.next().charAt(0) ;
+                case 5:
+                    System.out.println("Enter start index:");
+                    int start = scannerObj.nextInt();
+                    System.out.println("Enter length of splice:");
+                    int length = scannerObj.nextInt();
+                    String spliced = objMyString.spliceFunc(start, length);
+                    System.out.println("Result after splicing: " + spliced);
+                    break;
 
-                objMyString.replaceFunc(oldChar, newChar) ;
-                System.out.println("Result after replacing : " + objMyString.value) ;
-                break ;
+                case 6:
+                    System.out.println("Enter pattern to split:");
+                    String pattern_Str = scannerObj.nextLine();
+                    objMyString.splitFunc(pattern_Str);
+                    break;
 
+                case 7:
+                    objMyString.getMaxRepeatingChar();
+                    break;
 
-            // is palindrom choice
-            case 4:
-            boolean isPal = objMyString.isPalindrom() ;
-                if(isPal){
-                    System.out.println("String is palindrom") ;
-                }
-                else{
-                    System.out.println("String is not palindrom") ;
-                }
-               break ;
+                case 8:
+                    String sorted = objMyString.sortFunc();
+                    System.out.println("Result after sorting: " + sorted);
+                    break;
 
+                case 9:
+                    System.out.println("Enter number of elements to shift at end:");
+                    int n = scannerObj.nextInt();
+                    String shifted = objMyString.shiftFunc(n);
+                    System.out.println("Result after shifting: " + shifted);
+                    break;
 
-            // spice choice
-            case 5:
-                System.out.println("Enter start index: ") ;
-                int start = scannerObj.nextInt() ;
+                case 10:
+                    String reversed = objMyString.reverseFunc();
+                    System.out.println("Result after reversing: " + reversed);
+                    break;
 
-                System.out.println("Enter length of splice : ") ;
-                int length = scannerObj.nextInt() ;
+                case 11:
+                    System.out.println("Exiting program...");
+                    System.exit(0);
+                    break;
 
-                String spliced = objMyString.spliceFunc(start,length) ;
-                System.out.println("Result after splicing : " + spliced) ;
-                break ;
+                default:
+                    System.out.println("You entered an invalid choice");
+                    break;
+            }
 
-
-            // split choice
-            case 6:
-            System.out.println("Enter pattern to split:") ;
-            String Pattern_Str = scannerObj.nextLine() ;
-            objMyString.splitFunc(Pattern_Str) ;
-            break ;
-
-
-            // max repeating char choice
-            case 7:
-                objMyString.getMaxRepeatingChar() ;
-                break ;
-
-
-            // sort choice
-            case 8:
-                String sorted = objMyString.sortFunc() ;
-                System.out.println("Result after sorting : " + sorted) ;
-                break ;
-
-
-            // shift choice
-            case 9:
-               System.out.println("Enter number of elements to shift at end: ") ;
-               int n = scannerObj.nextInt() ;
-               String shifted = objMyString.shiftFunc(n) ;
-               System.out.println("Result after shifting : " + shifted) ;
-                break ;
-
-
-            // reverse choice
-            case 10:
-                String reversed = objMyString.reverseFunc() ;
-                System.out.println("Result after reversing : " + reversed) ;
-                break ;
-
-
-            // exit choice
-            case 11:
-                System.exit(0) ;
-                break ;
-
-                
-            // default case
-            default:
-                 System.out.println("You entered invalid choice") ;
-                 break ;
+        } catch (InputMismatchException e) {
+            System.out.println("Invalid input! Please enter a number for your choice.");
+        } catch (Exception e) {
+            System.out.println("An unexpected error occurred: " + e.getMessage());
+        } finally {
+            scannerObj.close();
         }
-        scannerObj.close() ;
-
     }
-};
+}
